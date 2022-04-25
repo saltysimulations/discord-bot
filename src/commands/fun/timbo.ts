266f6da1140e511389723992df75b0fd3417d020
Jordan Doose <peregrine.developments@gmail.com>
2022-04-25 13:22:46 -0700

    chore: added file headers

--- src/commands/fun/timbo.ts
+++ src/commands/fun/timbo.ts
@@ -1,3 +1,23 @@
+//============================================================
+//
+// Salty Simulations Discord Bot
+// Author: saltysimulations
+// URL: https://github.com/owen2007/Salty-Simulations-Discord-Bot/
+// Date: 2022-04-25
+// Version: 0.0.1
+// License: AGPLv3
+//
+// Source based on:
+// FlyByWire Discord Bot
+// Author: flybywire
+// URL: https://github.com/flybywiresim/discord-bot/
+//
+//============================================================
+//
+// TIMBOOOOOOOOOOOOOOOOOOOOOOOOOO
+//
+//============================================================
+
 import { makeEmbed } from '../../lib/embed';
 
 const TIMBO_URL: string = "https://cdn.discordapp.com/attachments/772533338820837437/966942411194515506/timbo_image.gif";
